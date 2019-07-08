6f8839762f507751da18aa2d4ff6070c1ad2febe
lyc <lyc1@erongdu.com>
2019-07-08 15:14:25 +0800

    课题3

--- course/src/three/Triangle.java
+++ course/src/three/Triangle.java
@@ -1,6 +1,46 @@
 package three;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Triangle {
 
+    public static String check(int a, int b, int c) {
+        //判断三角形 边长度排序， 假设为 a < b < c 要求a + b > c
+        if (a + b <= c
+                || a + c <= b
+                || b + c <= a
+        ) {
+            return "NO";
+        }
 
+        if (a == b && b == c) {
+            return "DB";
+        }
+
+        if (a == b || b == c || a == c) {
+            return "DY";
+        }
+
+        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
+            return "ZJ";
+        }
+
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入第一条边长");
+            int a = scanner.nextInt();
+            System.out.println("请输入第二条边长");
+            int b = scanner.nextInt();
+            System.out.println("请输入第三条边长");
+            int c = scanner.nextInt();
+            System.out.println(check(a, b, c));
+        } catch (InputMismatchException e) {
+            System.out.println("输入的三角形边长非整数！");
+        }
+    }
 }
